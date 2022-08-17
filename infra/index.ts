22d9309714a0502f74638b2643966bc165cc896d
Luis Lopez <lerl221295@gmail.com>
2022-08-17 12:45:56 -0300

    fix infra network

--- infra/index.ts
+++ infra/index.ts
@@ -3,19 +3,22 @@ import * as awsx from "@pulumi/awsx";
 
 // Create a container repository.
 const repo = new awsx.ecr.Repository("tasks-repo");
-
 // build and push the image to the repository
 const image = repo.buildAndPushImage("../")
 
-// Create a load balancer to listen for requests and route them to the container.
-const listener = new awsx.elasticloadbalancingv2.NetworkListener("tasks", { port: 80 });
+// Create a load balancer to listen for requests and route them to the container. The network LB will be created under the default VPC
+const networkLoadBalancer = new awsx.lb.NetworkLoadBalancer("tasks", { external: true });
+const target = networkLoadBalancer.createTargetGroup("tasks", { port: 8080 }); // app port
+const listener = target.createListener("tasks", { port: 80 }); // exposed port
 
-// Create an ECS Cluster
+
+// Create an ECS Cluster. This cluster will be
+// associated with the default VPC for the region.  To override that, pass in a VPC manually.
 const cluster = new awsx.ecs.Cluster("tasks-cluster");
 
 // Define the service, building and publishing our image, and using the load balancer.
 const service = new awsx.ecs.FargateService("tasks", {
-    cluster,
+    cluster, // if not sent, pulumi will know that a cluster is required, and it will create one for it
     taskDefinitionArgs: {
         containers: {
             tasks: {
