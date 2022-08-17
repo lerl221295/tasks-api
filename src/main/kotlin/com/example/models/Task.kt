7d4f2b58e70eba289361eddbe7e8c3ad3d8d8c65
Luis Lopez <lerl221295@gmail.com>
2022-08-17 13:02:03 -0300

    mock tasks

--- src/main/kotlin/com/example/models/Task.kt
+++ src/main/kotlin/com/example/models/Task.kt
@@ -6,8 +6,8 @@ data class Task(var id: Int, var tittle: String, var description: String, var pr
 
 var lastId = 0
 
-val taskStorage = mutableListOf(
-    Task(id = ++lastId, tittle = "Amar a mi esposo", description = "hacerle el amor muchas veces", priority = 1),
-    Task(id = ++lastId, tittle = "Amar a mi esposo", description = "hacerle el amor muchas veces", priority = 1),
-    Task(id = ++lastId, tittle = "Amar a mi esposo", description = "hacerle el amor muchas veces", priority = 1),
+val taskStorage = mutableListOf( // just some mock data to always start with something
+    Task(id = ++lastId, tittle = "Go shopping", description = "get the necessary food", priority = 1),
+    Task(id = ++lastId, tittle = "Cook dinner", description = "Make something delicious", priority = 1),
+    Task(id = ++lastId, tittle = "Load bills", description = "Load bills on the monthly report", priority = 1),
 )
