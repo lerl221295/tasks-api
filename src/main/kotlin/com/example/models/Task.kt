d6101c795a2fb43910bbd0c04677414247c47d01
Luis Lopez <lerl221295@gmail.com>
2022-08-18 15:13:19 -0300

    some emojis :)

--- src/main/kotlin/com/example/models/Task.kt
+++ src/main/kotlin/com/example/models/Task.kt
@@ -7,7 +7,7 @@ data class Task(var id: Int, var tittle: String, var description: String, var pr
 var lastId = 0
 
 val taskStorage = mutableListOf( // just some mock data to always start with something
-    Task(id = ++lastId, tittle = "Go shopping", description = "get the necessary food", priority = 1),
-    Task(id = ++lastId, tittle = "Cook dinner", description = "Make something delicious", priority = 1),
-    Task(id = ++lastId, tittle = "Load bills", description = "Load bills on the monthly report", priority = 1),
+    Task(id = ++lastId, tittle = "Go shopping", description = "get the necessary food :)", priority = 1),
+    Task(id = ++lastId, tittle = "Cook dinner", description = "Make something delicious :)", priority = 1),
+    Task(id = ++lastId, tittle = "Load bills", description = "Load bills on the monthly report :)", priority = 1),
 )
