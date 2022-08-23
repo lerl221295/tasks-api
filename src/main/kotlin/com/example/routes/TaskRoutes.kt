271a8966b0843f62fd5761e08245007000002d1c
Luis Lopez <lerl221295@gmail.com>
2022-08-23 15:04:21 -0300

    refactored endpoint and created service

--- src/main/kotlin/com/example/routes/TaskRoutes.kt
+++ src/main/kotlin/com/example/routes/TaskRoutes.kt
@@ -1,12 +1,9 @@
 package com.example.routes
-import aws.sdk.kotlin.runtime.auth.credentials.ProfileCredentialsProvider
-import aws.sdk.kotlin.services.s3.S3Client
-import aws.sdk.kotlin.services.s3.model.PutObjectRequest
-import aws.sdk.kotlin.services.s3.model.S3Exception
-import aws.smithy.kotlin.runtime.content.ByteStream
+
 import com.example.models.Task
 import com.example.models.lastId
 import com.example.models.taskStorage
+import com.example.services.uploadTaskImage
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
@@ -20,40 +17,6 @@ data class ErrorResponse(val errorMessage: String)
 
 @Serializable
 data class SuccessResponse(val message: String)
-
-
-suspend fun uploadImage(taskId: Int, file: ByteArray): String {
-    val s3Client = S3Client.fromEnvironment() {
-        credentialsProvider = ProfileCredentialsProvider(profileName = "personal")
-        region = "us-east-1"
-    }
-
-//    val metadataVal = mutableMapOf<String, String>()
-//    metadataVal["myVal"] = "test"
-
-    val fileName = "task-$taskId-image.jpg"
-
-    val request = PutObjectRequest {
-        bucket = "lerl221295-tasks-images"
-        key = fileName
-//        metadata = metadataVal
-        body = ByteStream.fromBytes(file)
-    }
-
-    try {
-        val response = s3Client.putObject(request)
-        println("Tag information is ${response.eTag}")
-        s3Client.close()
-    } catch (e: S3Exception) {
-        println("ERROR (S3Exception): ${e.sdkErrorMetadata.errorMessage}")
-    } catch(e: Exception) {
-        println("IDK")
-    }
-
-
-
-    return fileName
-}
 
 fun Route.taskRouting() {
     route("/tasks") {
@@ -100,30 +63,15 @@ fun Route.taskRouting() {
         }
 
         post("{id?}/image") {
-            var fileDescription = ""
-
             val id = call.parameters["id"]?.toInt() ?: return@post call.respond(HttpStatusCode.BadRequest)
             val task = taskStorage.find { it.id == id } ?: return@post call.respond(HttpStatusCode.NotFound, ErrorResponse("No task with id $id"))
 
             val multipartData = call.receiveMultipart()
+            val imageFormData = multipartData.readPart() as PartData.FileItem
+            val fileUrl = uploadTaskImage(id, imageFormData)
 
-            multipartData.forEachPart { part ->
-                when (part) {
-                    is PartData.FormItem -> {
-                        fileDescription = part.value
-                    }
-
-                    is PartData.FileItem -> {
-//                        fileName = part.originalFileName as String
-                        val fileName = uploadImage(id, part.streamProvider().readBytes())
-                        task.apply {
-                            imageUrl = fileName
-                        }
-//                        File("uploads/$fileName").writeBytes(fileBytes)
-                    }
-
-                    else -> {}
-                }
+            task.apply {
+                imageUrl = fileUrl
             }
 
             call.respond(HttpStatusCode.OK, SuccessResponse("Image saved successfully"))
